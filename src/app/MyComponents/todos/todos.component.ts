940e4b5262f3e322213686b77d8f12e3671000e4
Abhishek Garg <abhishekgarg@abhishek.garg.ext>
2021-05-19 13:02:06 +0530

    add todos component

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, } from '@angular/core';
+import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
+import {Todo} from "../../Todo";
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+
+  todos: Todo[];
+  constructor() { 
+    this.todos = [
+      {
+        sno: 1,
+        title: "Learn Angular",
+        desc: "start learning from YouTube",
+        active: true
+      },
+      {
+        sno: 2,
+        title: 'Mini-Project',
+        desc: 'Make mini-project using angular',
+        active: true
+      }
+    ]
+  }
+
+  ngOnInit(): void {
+  }
+  
+  onClick(todo:Todo) {
+    console.log("Description");
+  }
+
+}
---
+++ src/app/Todo.ts
@@ -0,0 +1,13 @@
+export class Todo{
+    sno: number
+    title: string
+    desc: string
+    active: boolean
+    constructor(){
+        this.sno = 1
+        this.title = "Title"
+        this.desc = "Description"
+        this.active = true
+
+    }
+}
