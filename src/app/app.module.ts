47bdb10d5a3f0917e0dc037dd562c904d6600443
Abhishek Garg <abhishekgarg.14august@gmail.com>
2021-05-21 10:45:36 +0530

    delete quote route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
 import { AddModalComponent } from './MyComponents/add-modal/add-modal.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { FinishTodosComponent } from './MyComponents/finish-todos/finish-todos.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    AddModalComponent
+    AddModalComponent,
+    
+    FinishTodosComponent
   ],
   imports: [
     BrowserModule,
